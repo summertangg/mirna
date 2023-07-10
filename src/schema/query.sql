b30441dd1c7ca0bd7d675b075dbd02ff7bccb30d
summertangg <summer.d39c@gmail.com>
2023-07-10 15:33:49 -0700

    200 Add venn diagram of experiment supported vs predicted targets

--- src/schema/query.sql
+++ src/schema/query.sql
@@ -1,12 +1,14 @@
 select * from mirnas where mirna_id = 'hsa-miR-155-3p'
 
-select * from genes
+select * from genes --2717
 
 select * from pathways
 
 
-select * from mirna_gene_pathway 
-where mirna = 'hsa-mir-18a-3p' and pathway = -1
+select mirna, gene from mirna_gene_pathway 
+where mirna = 'hsa-mir-155-5p' and pathway < 1
+group by mirna, gene
+order by gene
 
 
 
@@ -31,4 +33,30 @@ from mirna_gene_pathway
 group by gene, mirna
 order by gene
 
+-- get genes with the most interactions
+SELECT gene, COUNT(mirna) AS num_interaction, STRING_AGG(DISTINCT mirna, ',') AS grouped_mirna,
+  STRING_AGG(DISTINCT p.name, ',') AS grouped_pathways
+  -- ARRAY_TO_STRING(ARRAY_AGG(DISTINCT pathway), ',') AS grouped_pathway
+FROM mirna_gene_pathway AS t
+LEFT JOIN pathways AS p on p.pathway_id = t.pathway and p.pathway_id > 0
+GROUP BY gene
+ORDER BY num_interaction DESC;
 
+select * from mirna_gene_pathway
+where gene = 'BCL2' and mirna = 'hsa-mir-17-5p'
+
+
+-- get pathways with the most interactions
+SELECT pathway, p.name, COUNT(gene) AS num_interaction, 
+  COUNT(DISTINCT mirna) AS num_mirna, 
+  STRING_AGG(DISTINCT mirna, ',') AS grouped_mirna
+FROM mirna_gene_pathway AS t
+JOIN pathways AS p on p.pathway_id = t.pathway and p.pathway_id > 0
+GROUP BY pathway, p.name
+ORDER BY num_interaction DESC;
+
+
+select gene, count(mirna) from mirna_gene_pathway
+where pathway = 4
+group by gene
+order by gene
