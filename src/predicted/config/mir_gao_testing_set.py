42c0f8107033cc86790fa0c07ce3db28bcd71a2b
summertangg <summer.d39c@gmail.com>
2023-08-17 13:43:52 -0700

    620 New testing set

---
+++ src/predicted/config/mir_gao_testing_set.py
@@ -0,0 +1,16 @@
+# The datasets generated and/or analyzed during the current study are available in the Gene Expression Omnibus repository (GSE173080) (https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE173080).
+# Gao, Hai-Xia, et al. "Screening and identification of differentially expressed microRNAs in diffuse large B‑cell lymphoma based on microRNA microarray." Oncology Letters 22.5 (2021): 1-13.
+
+table_4 = [
+    "miR-19a-3p",
+    "miR-19b-3p",
+    "miR-193a-3p",
+    "miR-370-3p",
+    "miR-1275",
+    "miR-490-5p",
+    "miR-630",
+    "miR-665"
+]
+
+table_4_file = "mirdb_interactions_gao_testing_table_4.csv"
+
