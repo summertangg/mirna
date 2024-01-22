775839641a73721c66390e8f1206c1a8c51557af
summertangg <summer.d39c@gmail.com>
2024-01-21 19:36:21 -0800

    790 Add mean_mass attribute

--- src/predicted/export.py
+++ src/predicted/export.py
@@ -40,15 +40,17 @@ def export_interactions(db_conn, root_path, out_file, config=None):
     ratio_c_col = []
     ratio_g_col = []
     ratio_u_col = []
+    mass_col = []
     for index, row in pivot_df.iterrows():
         print(f"{index}: disease {mirnas_dict[index]}")
         if mirnas_dict[index] == 'DLBCL':
             disease_col.append('Yes')
-            num_of_base_col.append(num_of_bases(m54.sequence[index]))
+            num_of_base_col.append(len(m54.sequence[index]))
             ratio_a_col.append(frequency_of_base(m54.sequence[index], 'A'))
             ratio_c_col.append(frequency_of_base(m54.sequence[index], 'C'))
             ratio_g_col.append(frequency_of_base(m54.sequence[index], 'G'))
             ratio_u_col.append(frequency_of_base(m54.sequence[index], 'U'))
+            mass_col.append(mean_mass(m54.sequence[index]))
         else:
             disease_col.append('No')
             num_of_base_col.append(0)
@@ -56,6 +58,7 @@ def export_interactions(db_conn, root_path, out_file, config=None):
             ratio_c_col.append(0)
             ratio_g_col.append(0)
             ratio_u_col.append(0)
+            mass_col.append(0)
     
     # pivot_df.insert(0, 'DIAGNOSTIC_POSITIVE', disease_col)
     pivot_df['NUM_OF_BASES'] = num_of_base_col
@@ -63,6 +66,7 @@ def export_interactions(db_conn, root_path, out_file, config=None):
     pivot_df['RATIO_C'] = ratio_c_col
     pivot_df['RATIO_G'] = ratio_g_col
     pivot_df['RATIO_U'] = ratio_u_col
+    pivot_df['MEAN_MASS'] = mass_col
     pivot_df['DIAGNOSTIC_POSITIVE'] = disease_col
 
     # Write the pivoted DataFrame to a CSV file
@@ -72,18 +76,43 @@ def export_interactions(db_conn, root_path, out_file, config=None):
     print(f"{file_path} file saved successfully.")
 
 def num_of_bases(seq):
-    return len(seq)
-
-def frequency_of_base(seq, base):
-    total_base = len(seq)
-    count = 0
+    count_A = 0
+    count_C = 0
+    count_G = 0
+    count_U = 0
 
     # Iterate through the sequence
     for v in seq:
-        if v == base:
-            count += 1
-    ratio = "{:.2f}".format(count/total_base)
-    return ratio
+        if v == 'A':
+            count_A += 1
+        elif v == 'C':
+            count_C += 1
+        elif v == 'G':
+            count_G += 1
+        elif v == 'U':
+            count_U += 1
+    
+    base_count = {
+        "A": count_A,
+        "C": count_C,
+        "G": count_G,
+        "U": count_U
+    }
+
+    return base_count
+
+def frequency_of_base(seq, base):
+    total_base = len(seq)
+    base_count = num_of_bases(seq)
+    ratio = base_count[base]/total_base
+    return "{:.2f}".format(ratio)
+
+def mean_mass(seq):
+    # 135.1 (NA)+111.1 (NC)+151.1 (NG)+112.1 (NU)/N
+    total_base = len(seq)
+    base_count = num_of_bases(seq)
+    mass = (135.1 * base_count['A'] + 111.1 * base_count['C'] + 151.1 * base_count['G'] + 112.1 * base_count['U']) / total_base
+    return "{:.2f}".format(mass)
 
 root_path = "~/code/mirna/resources/results"
 conn = psycopg2.connect(database="postgres", user="postgres", password="1qaz2wsX", host="127.0.0.1", port="5432")
